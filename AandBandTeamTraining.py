e25f3a2b83d346181de1858b132f7246776ace40
mi-ran <azsxdc8911@gmail.com>
2018-10-07 20:10:28 +0900

    C.A_and_B_and_Team_Training 해결

---
+++ AandBandTeamTraining.py
@@ -0,0 +1,21 @@
+import sys
+
+e, n = list(map(int, input().split()))
+
+res = 0
+while True:
+    if e + n < 3:
+        print(res)
+        sys.exit()
+    if e is 0 or n is 0:
+        print(res)
+        sys.exit()
+    if e < n:
+        n = n - 2
+        e = e - 1
+        res = res + 1
+    else:
+        e = e - 2
+        n = n - 1
+        res = res + 1
+print(res)
