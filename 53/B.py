bc8c6e7df546e54c10d3aad0df4d9ea6d08ddaf6
mi-ran <azsxdc8911@gmail.com>
2018-11-20 23:30:08 +0900

    [20181120] Accept

--- 53/B.py
+++ 53/B.py
@@ -4,15 +4,16 @@ vasays = list(map(int, input().split()))
 res  = []
 b = [False for i in range(0, n + 1)]
 
+cur = 0
 for i in range(0, n):
     if b[vasays[i]]:
         res.append("0")
         continue
-    for j in range(0, len(bookStack)):
+    for j in range(cur, len(bookStack)):
         b[bookStack[j]] = True
         if vasays[i] == bookStack[j]:
-           bookStack = bookStack[j + 1 :]
-           res.append(str(j + 1))
+           res.append(str(j-cur + 1))
+           cur = j + 1
            break
 
 print(' '.join(res))
