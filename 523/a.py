344398ea2cda2954905af66c398795c0f9ee45be
mi-ran <azsxdc8911@gmail.com>
2018-11-27 22:24:20 +0900

    [20181127] 속도 개선(나눗셈 계산으로 반복문 줄임)

--- 523/a.py
+++ 523/a.py
@@ -2,9 +2,10 @@ n, S = list(map(int, input().split()))
 count = 0
 tmp = n
 while S > 0:
-    if S >= tmp:
-        S = S - tmp
-        count = count + 1
+    value = S//tmp
+    if value > 0:
+        S = S - tmp*value
+        count = count + value
     else:
         tmp = tmp - 1
 print(count)
